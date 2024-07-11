9c33ca100993c89b1549d7c2844586f7c684f423
Simon Wöhler <swoehler@dimedis.de>
2024-07-11 14:58:56 +0200

    solution with separate function

--- practice/src/main/kotlin/Main.kt
+++ practice/src/main/kotlin/Main.kt
@@ -13,14 +13,24 @@ fun main() {
 
     val randomList = List(10) { Random.nextInt(0, 10) }
     println("your random list is: $randomList")
+
+    val result = findIndex(randomList)
+    println(result)
+
+}
+
+fun findIndex(randomList: List<Int>): MutableList<Int> {
+    val list = mutableListOf<Int>()
+
     println("gimme a numba")
     val eingabe = readln().toInt()
 
-    for (x in randomList.indices) {
-        if (eingabe == randomList[x]) {
-            println("nummer an index: $x")
+    for (kaka in randomList.indices) {
+        if (eingabe == randomList[kaka]) {
+            list.add(kaka)
         }
     }
+    return list
 }
 
 
